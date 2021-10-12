e43e35cddb0c2cc368f2084375a7f13a8acb1b99
Peter <masonptr@gmail.com>
2021-10-12 15:33:04 +0100

    Demo commit

--- packages/edition-slices/src/slices/frontleadtwo/index.js
+++ packages/edition-slices/src/slices/frontleadtwo/index.js
@@ -1,5 +1,11 @@
-import React from "react";
-import { ScrollView, Text, useWindowDimensions, View } from "react-native";
+import React, { useState } from "react";
+import {
+  Button,
+  ScrollView,
+  Text,
+  useWindowDimensions,
+  View,
+} from "react-native";
 
 // import {
 //   TileGFront,
@@ -7,46 +13,82 @@ import { ScrollView, Text, useWindowDimensions, View } from "react-native";
 // } from "@times-components-native/edition-slices/src/tiles";
 // import InTodaysEdition from "@times-components-native/in-todays-edition";
 
-import { useResponsiveContext } from "@times-components-native/responsive";
+// import { useResponsiveContext } from "@times-components-native/responsive";
 //import { Orientation } from "@times-components-native/responsive/src/types";
 
 const FrontLeadTwo = (props) => {
-  const { orientation } = useResponsiveContext();
+  const scales = [
+    0.823,
+    0.882,
+    0.941,
+    1,
+    1.118,
+    1.235,
+    1.353,
+    1.786,
+    2.143,
+    2.643,
+    3.143,
+    3.571,
+  ];
+
+  const [scaleIndex, setScaleIndex] = useState(3);
+  const [showExtraContent, setShowExtraContent] = useState(false);
   const { width, fontScale } = useWindowDimensions();
-  const getInToday = (layout) =>
-    layout === orientation ? (
-      <View style={{ backgroundColor: "violet", width: "100%", height: 100 }}>
-        <Text>IN TODAY SECTION</Text>
-      </View>
-    ) : null;
 
   const getLayout = () => {
-    const breakpoint = orientation === "landscape" ? 960 : 600;
-    const mediaQuery = width / fontScale;
+    // const breakpoint = width / fontScale;
+    const breakpoint = width / scales[scaleIndex];
 
-    console.log("media Q: ", mediaQuery);
-    console.log("breakpoint: ", breakpoint);
+    // default layout - fontScale @ 1
+    const layout = {
+      colSize: "100%",
+      inTodaySize: "100%",
+      direction: "column",
+      size: "small",
+    };
 
-    let colSize = orientation === "landscape" ? "35%" : "50%";
-    let inTodaySize = orientation === "landscape" ? "100%" : "30%";
-    let direction = "row";
-
-    if (mediaQuery < breakpoint) {
-      colSize = "100%";
-      inTodaySize = "100%";
-      direction = "column";
+    if (breakpoint > 600 && breakpoint < 1080) {
+      layout.colSize = "50%";
+      layout.direction = "row";
+      layout.size = "medium";
     }
 
-    return {
-      colSize,
-      inTodaySize,
-      direction,
-    };
+    if (breakpoint >= 1080) {
+      layout.colSize = "35%";
+      layout.direction = "row";
+      layout.inTodaySize = "30%";
+      layout.size = "large";
+    }
+
+    return layout;
   };
 
   const layout = getLayout();
 
   console.log("LAYOUT: ", layout);
+
+  const getInToday = () => (
+    <View style={{ backgroundColor: "violet", width: "100%", height: 100 }}>
+      <Text>IN TODAY SECTION</Text>
+    </View>
+  );
+
+  const updateScaleIndex = (value) => {
+    let newIndex = scaleIndex;
+    newIndex += value;
+    if (newIndex < 0) {
+      newIndex = 0;
+    }
+    if (newIndex === scales.length) {
+      newIndex = scales.length - 1;
+    }
+    setScaleIndex(newIndex);
+  };
+
+  const toggleShowExtraContent = () => {
+    setShowExtraContent(!showExtraContent);
+  };
 
   return (
     <ScrollView
@@ -61,9 +103,63 @@ const FrontLeadTwo = (props) => {
     >
       <View style={{ flexDirection: layout.direction, flex: 1 }}>
         <View style={{ backgroundColor: "orange", width: layout.colSize }}>
-          <Text style={{ fontSize: 32 }}>A</Text>
-          <Text style={{ fontSize: 32 }}>B</Text>
-          <Text style={{ fontSize: 32 }}>C</Text>
+          <Text
+            style={{
+              fontSize: 18 * scales[scaleIndex],
+              marginVertical: 12,
+              marginHorizontal: 12,
+            }}
+          >
+            Current scale: {scales[scaleIndex]} Layout: {layout.size}
+          </Text>
+          <Text
+            style={{
+              fontSize: 18 * scales[scaleIndex],
+              marginVertical: 12,
+              marginHorizontal: 12,
+            }}
+          >
+            Current Layout: {layout.size}
+          </Text>
+          <Button title="Smaller" onPress={() => updateScaleIndex(-1)} />
+          <Button title="larger" onPress={() => updateScaleIndex(1)} />
+          <Button
+            title="toggle extra content"
+            onPress={() => toggleShowExtraContent()}
+          />
+          <Text
+            style={{ fontSize: 32 * scales[scaleIndex], marginHorizontal: 12 }}
+          >
+            Times Newspaper Headline
+          </Text>
+          {!showExtraContent ? null : (
+            <>
+              <Text style={{ fontSize: 32 }}>x</Text>
+              <Text style={{ fontSize: 32 }}>x</Text>
+              <Text style={{ fontSize: 32 }}>x</Text>
+              <Text style={{ fontSize: 32 }}>x</Text>
+              <Text style={{ fontSize: 32 }}>x</Text>
+              <Text style={{ fontSize: 32 }}>x</Text>
+              <Text style={{ fontSize: 32 }}>x</Text>
+              <Text style={{ fontSize: 32 }}>x</Text>
+              <Text style={{ fontSize: 32 }}>x</Text>
+              <Text style={{ fontSize: 32 }}>x</Text>
+              <Text style={{ fontSize: 32 }}>x</Text>
+              <Text style={{ fontSize: 32 }}>x</Text>
+              <Text style={{ fontSize: 32 }}>x</Text>
+              <Text style={{ fontSize: 32 }}>x</Text>
+              <Text style={{ fontSize: 32 }}>x</Text>
+              <Text style={{ fontSize: 32 }}>x</Text>
+              <Text style={{ fontSize: 32 }}>x</Text>
+              <Text style={{ fontSize: 32 }}>x</Text>
+              <Text style={{ fontSize: 32 }}>x</Text>
+              <Text style={{ fontSize: 32 }}>x</Text>
+              <Text style={{ fontSize: 32 }}>x</Text>
+              <Text style={{ fontSize: 32 }}>x</Text>
+              <Text style={{ fontSize: 32 }}>x</Text>
+            </>
+          )}
+          {/* <Text style={{ fontSize: 32 }}>x</Text>
           <Text style={{ fontSize: 32 }}>x</Text>
           <Text style={{ fontSize: 32 }}>x</Text>
           <Text style={{ fontSize: 32 }}>x</Text>
@@ -85,8 +181,7 @@ const FrontLeadTwo = (props) => {
           <Text style={{ fontSize: 32 }}>x</Text>
           <Text style={{ fontSize: 32 }}>x</Text>
           <Text style={{ fontSize: 32 }}>x</Text>
-          <Text style={{ fontSize: 32 }}>x</Text>
-          <Text style={{ fontSize: 32 }}>x</Text>
+          <Text style={{ fontSize: 32 }}>x</Text> */}
         </View>
         <View style={{ backgroundColor: "purple", width: layout.colSize }}>
           <Text style={{ fontSize: 32 }}>A</Text>
@@ -94,10 +189,10 @@ const FrontLeadTwo = (props) => {
           <Text style={{ fontSize: 32 }}>C</Text>
         </View>
         <View style={{ width: layout.inTodaySize }}>
-          {getInToday("landscape")}
+          {layout.size === "large" ? getInToday() : null}
         </View>
       </View>
-      {getInToday("portrait")}
+      {layout.size !== "large" ? getInToday() : null}
     </ScrollView>
   );
 };
