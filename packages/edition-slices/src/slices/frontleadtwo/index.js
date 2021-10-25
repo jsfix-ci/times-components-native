55b8a6af9e4bbee892524f3e36d04676078f696a
Peter <masonptr@gmail.com>
2021-10-25 17:56:07 +0100

    apply responsive layout based on scale and width

--- packages/edition-slices/src/slices/frontleadtwo/index.js
+++ packages/edition-slices/src/slices/frontleadtwo/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, View } from "react-native";
 import {
   TileGFront,
   TileHFront,
 } from "@times-components-native/edition-slices/src/tiles";
 import InTodaysEdition from "@times-components-native/in-todays-edition";
 import { CHundredFifty, RowWrapper } from "@times-components-native/layouts";
+import { MediaQuery } from "@times-components-native/hooks";
 
 const FrontLeadTwo = (props) => {
   const {
@@ -14,6 +15,29 @@ const FrontLeadTwo = (props) => {
     slice: { lead1, lead2 },
     inTodaysEditionSlice: { items: inTodaysEditionItems = [] },
   } = props;
+
+  const screenSize = MediaQuery();
+
+  console.log(screenSize);
+
+  const getLayout = () => {
+    const layout = {
+      direction: "column",
+      width: "100%",
+    };
+    switch (screenSize) {
+      case "EXTRA EXTRA LARGE":
+      case "EXTRA LARGE":
+      case "LARGE":
+        layout.direction = "row";
+        layout.width = "70%";
+
+        break;
+    }
+    return layout;
+  };
+
+  const layout = getLayout();
 
   return (
     <ScrollView
@@ -26,29 +50,52 @@ const FrontLeadTwo = (props) => {
       }}
     >
       <RowWrapper>
-        <CHundredFifty>
-          <TileHFront
-            onPress={onPress}
-            tile={lead1}
-            tileName="lead1"
-            orientation={"portrait"}
-          />
-        </CHundredFifty>
-        <CHundredFifty>
-          <TileGFront
-            onPress={onPress}
-            tile={lead2}
-            tileName="lead2"
-            orientation={"portrait"}
-          />
-        </CHundredFifty>
+        <View
+          style={{
+            width: layout.width,
+            borderColor: "red",
+            borderWidth: 1,
+            flexDirection: layout.direction,
+          }}
+        >
+          <RowWrapper>
+            <CHundredFifty>
+              <TileHFront
+                onPress={onPress}
+                tile={lead1}
+                tileName="lead1"
+                orientation={"portrait"}
+              />
+            </CHundredFifty>
+            <CHundredFifty>
+              <TileGFront
+                onPress={onPress}
+                tile={lead2}
+                tileName="lead2"
+                orientation={"portrait"}
+              />
+            </CHundredFifty>
+          </RowWrapper>
+        </View>
+        {layout.direction === "row" ? (
+          <View style={{ width: "30%" }}>
+            <InTodaysEdition
+              direction="column"
+              items={inTodaysEditionItems}
+              onArticlePress={onPress}
+              onLinkPress={onLinkPress}
+            />
+          </View>
+        ) : null}
       </RowWrapper>
-      <InTodaysEdition
-        direction="row"
-        items={inTodaysEditionItems}
-        onArticlePress={onPress}
-        onLinkPress={onLinkPress}
-      />
+      {layout.direction === "column" ? (
+        <InTodaysEdition
+          direction="row"
+          items={inTodaysEditionItems}
+          onArticlePress={onPress}
+          onLinkPress={onLinkPress}
+        />
+      ) : null}
     </ScrollView>
   );
 };
