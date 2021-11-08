c9bb502646ce5e5c3bf39bcc7e7942d59953bd14
Peter <masonptr@gmail.com>
2021-11-08 15:43:20 +0000

    remove dead styles for tile a front

--- packages/edition-slices/src/tiles/tile-a-front/styles/index.js
+++ packages/edition-slices/src/tiles/tile-a-front/styles/index.js
@@ -6,62 +6,23 @@ import {
   colours,
 } from "@times-components-native/styleguide";
 
-const summary = {
-  ...globalSpacingStyles.tabletTeaser,
-};
-
-const headline = {
-  ...globalSpacingStyles.tabletHeadline,
-  fontFamily: fonts.headline,
-};
-
-const strapline = {
-  fontFamily: fonts.headlineRegular,
-  color: colours.functional.primary,
-  fontSize: 24,
-  lineHeight: 24,
-};
-
-const sharedStyles = {
+const styles = StyleSheet.create({
   container: {
     paddingBottom: spacing(0),
     paddingRight: spacing(2),
     flex: 1,
   },
+  headline: {
+    ...globalSpacingStyles.tabletHeadline,
+    fontFamily: fonts.headline,
+    fontSize: 42,
+    lineHeight: 42,
+  },
   summary: {
-    ...summary,
+    ...globalSpacingStyles.tabletTeaser,
     fontSize: 14,
     lineHeight: 18,
   },
-  strapline,
-};
-
-const sharedLandscapeStyles = {
-  ...sharedStyles,
-  summaryContainer: {
-    position: "absolute",
-    bottom: 0,
-    backgroundColor: colours.functional.white,
-    width: "100%",
-    paddingTop: spacing(2),
-  },
-  headlineMarginBottom: spacing(2),
-  straplineMarginTop: spacing(2),
-  straplineMarginBottom: spacing(3),
-  bylineMarginBottom: spacing(3),
-  imageContainer: {
-    width: "100%",
-    marginBottom: 0,
-  },
-  summary: {
-    ...summary,
-    fontSize: 14,
-    lineHeight: 20,
-  },
-};
-
-const sharedPortraitStyles = {
-  ...sharedStyles,
   headlineMarginBottom: spacing(4),
   straplineMarginTop: spacing(2),
   straplineMarginBottom: spacing(3),
@@ -70,157 +31,12 @@ const sharedPortraitStyles = {
     width: "100%",
     marginBottom: spacing(2),
   },
-};
-
-const portrait834 = {
-  ...sharedPortraitStyles,
-  summary: {
-    ...summary,
-    fontSize: 14,
-    lineHeight: 18,
+  strapline: {
+    fontFamily: fonts.headlineRegular,
+    color: colours.functional.primary,
+    fontSize: 24,
+    lineHeight: 24,
   },
-};
-
-const s = {
-  landscape: {
-    "1024": {
-      ...sharedLandscapeStyles,
-      headlineMarginBottom: spacing(2),
-      straplineMarginTop: spacing(2),
-      straplineMarginBottom: spacing(3),
-      headline: {
-        ...headline,
-        fontSize: 42,
-        lineHeight: 42,
-      },
-    },
-    "1080": {
-      ...sharedLandscapeStyles,
-      headlineMarginBottom: spacing(2),
-      straplineMarginTop: spacing(2),
-      straplineMarginBottom: spacing(3),
-      headline: {
-        ...headline,
-        fontSize: 45,
-        lineHeight: 45,
-      },
-    },
-    "1112": {
-      ...sharedLandscapeStyles,
-      headlineMarginBottom: spacing(3),
-      straplineMarginTop: spacing(2),
-      straplineMarginBottom: spacing(3),
-      headline: {
-        ...headline,
-        fontSize: 45,
-        lineHeight: 45,
-      },
-    },
-    "1133": {
-      ...sharedLandscapeStyles,
-      headlineMarginBottom: spacing(2),
-      straplineMarginTop: spacing(2),
-      straplineMarginBottom: spacing(3),
-      headline: {
-        ...headline,
-        fontSize: 45,
-        lineHeight: 45,
-      },
-    },
-    "1366": {
-      ...sharedLandscapeStyles,
-      headlineMarginBottom: spacing(3),
-      straplineMarginTop: spacing(2),
-      straplineMarginBottom: spacing(3),
-      headline: {
-        ...headline,
-        fontSize: 55,
-        lineHeight: 55,
-      },
-      summary: {
-        ...summary,
-        fontSize: 15,
-        lineHeight: 20,
-      },
-    },
-  },
-  portrait: {
-    "744": {
-      ...sharedPortraitStyles,
-      headline: {
-        ...headline,
-        fontSize: 42,
-        lineHeight: 42,
-      },
-    },
-    "768": {
-      ...sharedPortraitStyles,
-      headline: {
-        ...headline,
-        fontSize: 42,
-        lineHeight: 42,
-      },
-    },
-    "810": {
-      ...sharedPortraitStyles,
-      headline: {
-        ...headline,
-        fontSize: 45,
-        lineHeight: 45,
-      },
-    },
-    "834": {
-      ratios: {
-        0: {
-          ...portrait834,
-          headline: {
-            ...headline,
-            fontSize: 48,
-            lineHeight: 48,
-          },
-        },
-        0.75: {
-          ...portrait834,
-          headline: {
-            ...headline,
-            fontSize: 45,
-            lineHeight: 45,
-          },
-        },
-      },
-    },
-    "1024": {
-      ...sharedPortraitStyles,
-      imageContainer: {
-        ...sharedPortraitStyles.imageContainer,
-        marginBottom: spacing(3),
-      },
-      headline: {
-        ...headline,
-        fontSize: 55,
-        lineHeight: 55,
-      },
-      summary: {
-        ...summary,
-        fontSize: 15,
-        lineHeight: 18,
-      },
-    },
-  },
-};
-
-const styles = StyleSheet.create({
-  container: {
-    paddingBottom: spacing(0),
-    paddingRight: spacing(2),
-    flex: 1,
-  },
-  summary: {
-    ...summary,
-    fontSize: 14,
-    lineHeight: 18,
-  },
-  strapline,
 });
 
 export default styles;
