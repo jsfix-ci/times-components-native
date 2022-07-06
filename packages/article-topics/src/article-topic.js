75a8f91175bc00127094458cd032553a5cd556e4
Peter Mason <masonptr@gmail.com>
2022-07-06 10:17:48 +0100

    Fix/tnlt 9380 remove tool tips (#642)
    * resolve conflict
    
    * update package.json
    
    * remove tool tips from comments and topics
    
    * update snap shots

--- packages/article-topics/src/article-topic.js
+++ packages/article-topics/src/article-topic.js
@@ -1,36 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import Context from "@times-components-native/context";
 import { Text, View } from "react-native";
 import Link from "@times-components-native/link";
-import Tooltip from "@times-components-native/tooltip";
-import { useResponsiveContext } from "@times-components-native/responsive";
 import { withTrackEvents } from "@times-components-native/tracking";
 import styles from "./styles";
 import { topicDefaultProps, topicPropTypes } from "./article-topic-prop-types";
 
-const ArticleTopic = ({
-  articleId,
-  fontSize,
-  index,
-  lineHeight,
-  name,
-  onPress,
-  onTooltipPresented,
-  slug,
-  tooltipDisplayedInView,
-  tooltips,
-}) => {
+const ArticleTopic = ({ fontSize, lineHeight, name, onPress, slug }) => {
   const fontSizeStyle = fontSize ? { fontSize } : null;
   const lineHeightStyle = lineHeight ? { lineHeight } : null;
-  const { isArticleTablet } = useResponsiveContext();
-
-  const tooltipType = "topics";
-  const showTooltip = index === 0 && tooltips.includes(tooltipType);
-  const [isHighlighted, setIsHighlighted] = useState(showTooltip);
-
-  const unhighlightTopic = () => {
-    setIsHighlighted(false);
-  };
 
   const articleTopic = (
     <Context.Consumer>
@@ -41,12 +19,7 @@ const ArticleTopic = ({
             url={makeTopicUrl({ slug })}
             testIDProp="topic"
           >
-            <View
-              style={[
-                styles.container,
-                isHighlighted && styles.borderHighlight,
-              ]}
-            >
+            <View style={[styles.container, styles.borderHighlight]}>
               <Text
                 accessibilityComponentType="button"
                 accessibilityRole="button"
@@ -62,24 +35,7 @@ const ArticleTopic = ({
     </Context.Consumer>
   );
 
-  const articleTopicWithTooltip = (
-    <Tooltip
-      articleId={articleId}
-      content={<Text>Tap a topic to see more of our coverage</Text>}
-      displayedInView={tooltipDisplayedInView}
-      offsetY={5}
-      onClose={unhighlightTopic}
-      onTooltipPresented={onTooltipPresented}
-      placement="top"
-      type={tooltipType}
-      tooltips={tooltips}
-      width={isArticleTablet ? 236 : 207}
-    >
-      {articleTopic}
-    </Tooltip>
-  );
-
-  return showTooltip ? articleTopicWithTooltip : articleTopic;
+  return articleTopic;
 };
 
 ArticleTopic.propTypes = topicPropTypes;
