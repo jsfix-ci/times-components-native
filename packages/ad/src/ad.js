75608f405b0d5c35f191dfd5324d0a1fd10aa1f8
Peter Mason <masonptr@gmail.com>
2022-08-24 18:06:25 +0100

    lint and typecheck

--- packages/ad/src/ad.js
+++ packages/ad/src/ad.js
@@ -112,10 +112,9 @@ export class AdBase extends PureComponent {
       width,
     } = this.props;
 
-    const { hasError, isAdReady, offline } = this.state;
+    const { hasError, offline } = this.state;
     const config = getSlotConfig(slotName, width || screenWidth, orientation);
     const data = determineData(config, this.props);
-    console.log("CONFIG", config);
 
     if (hasError || offline) return null;
 
