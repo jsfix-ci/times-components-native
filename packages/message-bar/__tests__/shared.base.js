3be064fa17dc3a7ef241e25827eb373e1b43a627
Peter Mason <masonptr@gmail.com>
2022-08-24 18:01:29 +0100

    remove tests

--- packages/message-bar/__tests__/shared.base.js
+++
@@ -1,9 +0,0 @@
-import { iterator } from "@times-components-native/test-utils";
-import MessageBarTests from "./message-bar.base";
-import MessageManagerTests from "./message-manager.base";
-
-export default (animate) => {
-  const tests = [...MessageBarTests(animate), ...MessageManagerTests(animate)];
-
-  iterator(tests);
-};
