2113a5a6d70f36201115f6e978ed5ea803f21583
jaglop <jaglop@users.no.reply.github.com>
2022-09-02 17:56:59 +0200

    Upgrade files for RN 0.69, WIP on iOS

--- ios/TimesComponents/AppDelegate.mm
+++ ios/TimesComponents/AppDelegate.mm
@@ -18,6 +18,8 @@
 
 #import <react/config/ReactNativeConfig.h>
 
+static NSString *const kRNConcurrentRoot = @"concurrentRoot";
+
 @interface AppDelegate () <RCTCxxBridgeDelegate, RCTTurboModuleManagerDelegate> {
   RCTTurboModuleManager *_turboModuleManager;
   RCTSurfacePresenterBridgeAdapter *_bridgeAdapter;
@@ -69,7 +71,8 @@ static void InitializeFlipper(UIApplication *application) {
 #endif
 
   //TODO JAIME: I believe we do not need this as there is no root view
-  // UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"RnDiffApp", nil);
+  // NSDictionary *initProps = [self prepareInitialProps];
+  // UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"TimesComponents", initProps);
 
   // if (@available(iOS 13.0, *)) {
   //   rootView.backgroundColor = [UIColor systemBackgroundColor];
@@ -85,6 +88,25 @@ static void InitializeFlipper(UIApplication *application) {
 
   [ReactNativeNavigation bootstrapWithDelegate:self launchOptions:launchOptions];
   return YES;
+}
+
+/// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
+///
+/// @see: https://reactjs.org/blog/2022/03/29/react-v18.html
+/// @note: This requires to be rendering on Fabric (i.e. on the New Architecture).
+/// @return: `true` if the `concurrentRoot` feture is enabled. Otherwise, it returns `false`.
+- (BOOL)concurrentRootEnabled
+{
+  // Switch this bool to turn on and off the concurrent root
+  return false;
+}
+- (NSDictionary *)prepareInitialProps
+{
+  NSMutableDictionary *initProps = [NSMutableDictionary new];
+#ifdef RCT_NEW_ARCH_ENABLED
+  initProps[kRNConcurrentRoot] = @([self concurrentRootEnabled]);
+#endif
+  return initProps;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
