2c3b0532a839c5d9c9bad82b9a1ba71f5ca8b918
Josh Oldham <oldham.josh@gmail.com>
2022-09-23 16:13:57 +0100

    feat: update header now shows meridiems and date

--- packages/article-update-header/index.tsx
+++ packages/article-update-header/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 import { DateTime } from "luxon";
 import { ArticleFlags } from "@times-components-native/article-flag";
 import { ResponsiveContext } from "@times-components-native/responsive";
-import styleguide from "@times-components-native/styleguide";
+import styleguide, { spacing } from "@times-components-native/styleguide";
 
 interface IProps {
   breaking?: boolean;
@@ -16,6 +16,8 @@ function ArticleUpdateHeader({ breaking = false, headline, updated }: IProps) {
   const locale = "en-GB";
   const userZone = DateTime.local().zoneName;
   const dt = DateTime.fromISO(updated).setLocale(locale).setZone(defaultZone);
+  const now = DateTime.fromISO(new Date().toISOString());
+  const isToday = dt.hasSame(now, "day");
   const { colours, fonts } = styleguide();
 
   const getHeadlineSafely = () => {
@@ -43,40 +45,81 @@ function ArticleUpdateHeader({ breaking = false, headline, updated }: IProps) {
                 alignItems: "center",
                 flexDirection: "row",
                 marginBottom: 16,
+                justifyContent: "space-between",
+                flexWrap: "wrap",
+                marginTop: -4,
               }}
             >
-              {breaking && (
-                <View style={{ marginRight: 8 }}>
-                  <ArticleFlags
-                    color={colours.functional.darkRed}
-                    flags={[{ type: "BREAKING", expiryTime: null }]}
-                    style={{ marginBottom: 0 }}
-                  />
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  paddingRight: spacing(2),
+                  marginTop: 8,
+                }}
+              >
+                {breaking && (
+                  <View style={{ marginRight: 8 }}>
+                    <ArticleFlags
+                      color={colours.functional.darkRed}
+                      flags={[{ type: "BREAKING", expiryTime: null }]}
+                      style={{ marginBottom: 0 }}
+                    />
+                  </View>
+                )}
+
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                  }}
+                >
+                  <Text
+                    style={{
+                      color: colours.functional.brandColour,
+                      fontFamily: fonts.supporting,
+                    }}
+                  >
+                    {dt.toRelative()} |{" "}
+                  </Text>
+                  <Text
+                    style={{
+                      color: colours.functional.secondary,
+                      fontFamily: fonts.supporting,
+                    }}
+                  >
+                    {`${dt.toFormat("h:mma").toLocaleLowerCase()} ${
+                      userZone !== defaultZone ? dt.offsetNameShort : ""
+                    }`}
+                  </Text>
+                </View>
+              </View>
+              {!isToday && (
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    marginTop: 8,
+                  }}
+                >
+                  <Text
+                    style={{
+                      color: colours.functional.secondary,
+                      fontFamily: fonts.supporting,
+                    }}
+                  >
+                    {`${dt.toLocaleString({ month: "long" })} `}
+                  </Text>
+                  <Text
+                    style={{
+                      color: colours.functional.secondary,
+                      fontFamily: fonts.supporting,
+                    }}
+                  >
+                    {`${dt.toLocaleString({ day: "2-digit" })}`}
+                  </Text>
                 </View>
               )}
-
-              <View style={{ flexDirection: "row", alignItems: "center" }}>
-                <Text
-                  style={{
-                    color: colours.functional.brandColour,
-                    fontFamily: fonts.supporting,
-                  }}
-                >
-                  {dt.toRelative()} |{" "}
-                </Text>
-                <Text
-                  style={{
-                    color: colours.functional.secondary,
-                    fontFamily: fonts.supporting,
-                  }}
-                >
-                  {`${dt
-                    .toLocaleString(DateTime.TIME_24_SIMPLE)
-                    .toLocaleLowerCase()} ${
-                    userZone !== defaultZone ? dt.offsetNameShort : ""
-                  }`}
-                </Text>
-              </View>
             </View>
             <Text
               style={{
