a623eecfa10129928dbd3b6979ade567411c4c33
jaglop <jaglop@users.no.reply.github.com>
2022-11-01 21:31:19 +0100

    Bump gradle tools for lib

--- android/app/src/main/java/com/timescomponents/MainActivity.java
+++ android/app/src/main/java/com/timescomponents/MainActivity.java
@@ -4,5 +4,12 @@ import com.reactnativenavigation.NavigationActivity;
 
 public class MainActivity extends NavigationActivity {
 
-  
+//TODO on nuk-android https://raw.githubusercontent.com/react-native-community/rn-diff-purge/release/0.69.6/RnDiffApp/android/app/src/main/java/com/rndiffapp/MainActivity.java
+//Basically add the below
+  @Override
+  protected boolean isConcurrentRootEnabled() {
+    // If you opted-in for the New Architecture, we enable Concurrent Root (i.e. React 18).
+    // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
+    return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+  }
 }
