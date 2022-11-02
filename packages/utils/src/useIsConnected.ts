c91a0510c108d0bcd536824237a67f2f2114ee7c
jaglop <jaglop@users.no.reply.github.com>
2022-11-02 09:32:12 +0100

    Bump rn-netinfo to avoid androidx resource compilation error
    com.reactnativecommunity.netinfo:color/androidx_core_secondary_text_default_material_light) not found

--- packages/utils/src/useIsConnected.ts
+++ packages/utils/src/useIsConnected.ts
@@ -1,15 +1,13 @@
 import { useEffect, useState } from "react";
 import NetInfo from "@react-native-community/netinfo";
 
+//Notice it returns null if 'unknown' since https://github.com/react-native-netinfo/react-native-netinfo/compare/v5.9.10...v6.0.0
 export const useIsConnected = () => {
   const [isConnected, setIsConnected] = useState<boolean | null>(true);
-  useEffect(() => {
-    const unsubscribe = NetInfo.addEventListener(state => {
-      setIsConnected(state.isConnected);
-    });
-
-    return () => unsubscribe();
-  }, []);
+  useEffect(
+    () => NetInfo.addEventListener(state => setIsConnected(state.isConnected)),
+    [],
+  );
 
   return isConnected;
 };
